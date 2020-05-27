c3e63b6fdc5931fb27103764a0ef6b621a08c965
nicoJaredGov <ngovindsamy18@gmail.com>
2020-05-27 16:11:48 +0200

    initial files

---
+++ java/com/example/newproject2020/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.newproject2020;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.project2020.R;
+
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextView;
+    private EditText mUsername;
+    private EditText mPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTextView = findViewById(R.id.textView);
+        mUsername = findViewById(R.id.usernameInput);
+        mPassword = findViewById(R.id.passwordInput);
+    }
+
+    public void button_click(View view){
+        OkHttpClient client = new OkHttpClient();
+
+
+        String username = mUsername.getText().toString();
+
+        String url = "https://lamp.ms.wits.ac.za/home/s2094785/user_list.php?user=" + username;
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                if(response.isSuccessful()){
+                    final String myResponse = response.body().string();
+                    try {
+                        JSONArray passwordResponse = new JSONArray(response);
+                        JSONObject item =  passwordResponse.getJSONObject(0);
+                        String pass = item.getString("PASSWORD");
+                        mTextView.setText(pass);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+
+                        }
+                    });
+                }
+            }
+        });
+    }
+}
