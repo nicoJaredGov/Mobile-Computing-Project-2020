4279aca1e03c83c65d1943fd489b57aa826f342c
nicoJaredGov <ngovindsamy18@gmail.com>
2020-05-30 16:07:25 +0200

    Jared7 login done

--- java/com/example/newproject2020/MainActivity.java
+++ java/com/example/newproject2020/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.ContentValues;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -28,12 +29,16 @@ import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView testView;
+
     private TextView mTextView;
     private EditText mUsername;
     private EditText mPassword;
+    Button loginBtn;
     String username;
     String password;
     String pos;
+    Integer passwordCounter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,14 +47,21 @@ public class MainActivity extends AppCompatActivity {
         mTextView = findViewById(R.id.loginTextView);
         mUsername = findViewById(R.id.usernameInput);
         mPassword = findViewById(R.id.passwordInput);
-        username = mUsername.getText().toString();
-        password = mPassword.getText().toString();
+        loginBtn = (Button) findViewById(R.id.LoginBtn);
+
+        passwordCounter = 3; //counts down password attempts - only 3 allowed
+
+        testView = findViewById(R.id.testView);
     }
 
     public void login_button_click(View view){
+        username = mUsername.getText().toString();
+        password = mPassword.getText().toString();
+        int usernameInt = Integer.parseInt(username);
+
         PHPRequest loginReq = new PHPRequest("https://lamp.ms.wits.ac.za/home/s2067058/");
         ContentValues cv = new ContentValues();
-        //cv.put("user",username);
+        cv.put("user",usernameInt);
 
         if (getIntent().hasExtra("userType")){
             pos = getIntent().getExtras().getString("userType"); // 1 = customer, 2 = employee
@@ -63,16 +75,31 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
-        loginReq.doRequest(MainActivity.this, "order.php", cv, new RequestHandler() {
+        loginReq.doRequest(MainActivity.this, file, cv, new RequestHandler() {
             @Override
             public void processResponse(String response) throws JSONException {
-                JSONArray ja = new JSONArray(response);
-                mTextView.setText("HELLO");
+                processJSON(response);
             }
         });
 
+    }
 
-
+    public void processJSON(String r) throws JSONException {
+        JSONArray ja = new JSONArray(r);
+        JSONObject jo = ja.getJSONObject(0);
+        testView.setText(jo.getString("PASSWORD"));
+        if (password.equals(jo.getString("PASSWORD"))){
+            mTextView.setText("Correct!");
+            //next activity
+        }
+        else{
+            passwordCounter--;
+            if (passwordCounter == 0){
+                loginBtn.setEnabled(false);
+            }
+            mTextView.setText("You have "+passwordCounter.toString() + " attempts left.");
+            return;
+        }
 
     }
 }
