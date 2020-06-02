f7d0afbf537edd0a87ba0d7d21ea46ca090cef41
Shahil-Harripersad <2094785@students.wits.ac.za>
2020-06-02 14:00:18 +0200

    shahil 8

---
+++ java/com/example/newproject2020/Customer/CustomerSettingsActivity.java
@@ -0,0 +1,16 @@
+package com.example.newproject2020.Customer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.project2020.R;
+
+public class CustomerSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_settings);
+    }
+}
