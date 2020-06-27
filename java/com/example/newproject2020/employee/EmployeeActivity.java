18d21079a2f2ef62c771677146358670d57369f6
Shahil-Harripersad <2094785@students.wits.ac.za>
2020-06-27 20:39:47 +0200

    shahil 14

--- java/com/example/newproject2020/employee/EmployeeActivity.java
+++ java/com/example/newproject2020/employee/EmployeeActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -123,4 +125,6 @@ public class EmployeeActivity extends AppCompatActivity {
             return fragmentTitle.get(position);
         }
     }
+
+
 }
