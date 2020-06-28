27b2be457c1ac65d0a9927f2f66c05c7d2d9ca77
Shahil-Harripersad <2094785@students.wits.ac.za>
2020-06-28 19:48:44 +0200

    Shahil 16

--- java/com/example/newproject2020/customer/CustomerSettingsActivity.java
+++ java/com/example/newproject2020/customer/CustomerSettingsActivity.java
@@ -2,7 +2,9 @@ package com.example.newproject2020.customer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.project2020.R;
 
@@ -12,5 +14,10 @@ public class CustomerSettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_settings);
+    }
+
+    public void backOnClick(View view) {
+        Intent i = new Intent();
+        
     }
 }
